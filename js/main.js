e412f624ce883adad3cd4314ae0b0ad59f5102ab
sllorens-cuenca <sllorens.cuenca@gmail.com>
2025-01-22 12:01:14 +0100

    practice with elements dom

--- js/main.js
+++ js/main.js
@@ -42,3 +42,57 @@ all.forEach((element) => {
     element.style.color = "gray"
 })
 
+
+
+/// Context: document vs element
+
+//const allParagraphsInProductsElm = document.querySelectorAll("products p")
+
+const productsElm = document.getElementById("products")
+
+const allParagraphsInProductsElm = productsElm.getElementsByTagName("p") //all p in productsElm
+
+
+
+/// Properties
+
+const pikachuElm = document.getElementById("pikachu")
+//console.log(pikachuElm.innerHTML);
+
+pikachuElm.innerHTML = "<p> THIS IS THE NEW CONTENT </p>"
+
+
+//Read/modify text content
+
+const linkElm = document.getElementById("my-link")
+linkElm.innerText = "Aboutus -- we are";
+
+//read/modify Css 
+mainTitle.style.color = "blue"
+mainTitle.style.margin = "20px"
+mainTitle.style.backgroundColor = "yellow"
+mainTitle.style.border = "2px solid green"
+
+//read/modify the id --> elm.id
+mainTitle.id = "this-is-the-new-id"
+
+//read/modify the class --> elm.className
+mainTitle.className = "title rounded"
+
+// elm.classList
+
+mainTitle.classList.remove("rounded")
+mainTitle.classList.add("shadow")
+mainTitle.classList.toggle("active")
+
+
+// atributes
+
+// get: elm.getAttribute(attributeName)
+const result = linkElm.getAttribute("href")
+
+//modify or create: elm.setAttribute(name, value)
+linkElm.setAttribute("href", "https://ironhack.com")
+linkElm.setAttribute("target", "_blank")
+
+//remove elm.removeAttribute(attrName)
